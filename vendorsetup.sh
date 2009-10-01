6dfb6d665142140a7bbc6a65504c05f6fe40ac43
Jean-Baptiste Queru <jbq@google.com>
2009-10-01 13:47:20 -0700

    Add userdebug build in the menu

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
+add_lunch_combo aosp_dream-userdebug
 add_lunch_combo aosp_dream-eng
 
